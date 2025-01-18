d4d448e6ce41ec88a217220c24a119d93fa17bc5
Tanay Sharma <thisistanaysharma@gmail.com>
2025-01-18 16:22:33 +0530

    now added that zod checks in http-backend index.ts

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -3,21 +3,41 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
 const app = express();
 
 app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
   res.json({
     userId: "123",
   });
 });
 
 app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
   res.json({ token });
 });
 
 app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: "Invalid data" });
+    return;
+  }
   res.json({
     roomId: 123,
   });
